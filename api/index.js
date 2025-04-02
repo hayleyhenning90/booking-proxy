6b2f3e20ae32c68b5da302649c8a421f62f4fd0a
hayleyhenning90 <hayley@characters.io>
2025-04-01 19:14:06 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 export default async function handler(req, res) {
-  const response = await fetch('https://script.google.com/macros/s/AKfycbwwABcxE7zqSWO2QnTjRT1YV7ivZK3S0zb5QM8-rnVGlnE8nd8VdjH10MI1HsbbIxbX/exec');
-  const data = await response.json();
+  try {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbwwABcxE7zqSWO2QnTjRT1YV7ivZK3S0zb5QM8-rnVGlnE8nd8VdjH10MI1HsbbIxbX/exec");
 
-  res.status(200).json(data);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.status}`);
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message || "Unknown error" });
+  }
 }
